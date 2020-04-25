1acad7a233d42e1f103319b5c51e2cc6978b5d09
David <dnisenbaum1@gmail.com>
2020-04-25 13:26:10 -0400

    fuck you firebase

--- database/db.js
+++ database/db.js
@@ -1,15 +1,14 @@
 import { database } from "./firebase";
 
-//const db = database().firestore();
+const db = database().firestore();
 
 export function fetchGames() {
-    database.collection("games")
+    db.collection("games")
         .get()
         .then((games) => {
-            //games.forEach((g) => {
-            //    console.log(g)
-            //});
-            return games;
+            games.forEach((g) => {
+                console.log(g.data())
+            });
         })
         .catch((error) => {
             console.log(error);
