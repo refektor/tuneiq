78b25963d61c9a68b10690554d29b7901794996e
David <dnisenbaum1@gmail.com>
2020-04-25 15:50:47 -0400

    remove jest shit

--- database/__tests__/db-test.tsx
+++
@@ -1,8 +0,0 @@
-import * as db from '../db'
-
-test('fetchQuetions Succeeds', () => {
-    // Render a checkbox with label in the document
-    //db.fetchGames().then(result => {
-    //    console.log(result);
-    //});
-});
