5818756d588ff0f9570d5eb2fa1c2837fed5c09b
Anthony Fekete <afekete3@uwo.ca>
2020-05-10 16:26:34 -0400

    implemented increase score function

--- firebase/server.ts
+++ firebase/server.ts
@@ -2,33 +2,33 @@ import { getFirebaseApp } from './firebase'
 import { generateGameContent, GameDetails } from './game_content'
 
 
-function createGame(gameName: string, gamePassword: string, gameGenre: string, hostName: string, hostId: string) {
-    return getFirebaseApp().firestore().collection("games").where("name", "==", gameName).get().then(games => {
-        console.log(games.empty)
-        if (games.empty) {
-            console.log('weoo')
-            const gameContent = generateGameContent(gameGenre, hostId, hostName, gameName, gamePassword)
-            return gameContent.then((details: GameDetails) => {
-                console.log(details)
-                return getFirebaseApp().firestore().collection("games")
-                    .add(details)
-                    .then((docRef) => {
-                        console.log(docRef)
-                        console.log("succesfully created game with id: ", docRef.id);
-                        return docRef.id
-                    })
-                    .catch((error) => {
-                        console.log(error);
+function createGame(gameName: string, gamePassword: string, gameGenre: string, hostName: string, hostId: string): Promise<string> {
+    return getFirebaseApp().firestore()
+        .collection("games")
+        .where("name", "==", gameName)
+        .get().then(games => {
+            if (games.empty) {
+                return generateGameContent(gameGenre, hostId, hostName, gameName, gamePassword)
+                    .then((details: GameDetails) => {
+                        return getFirebaseApp().firestore()
+                            .collection("games")
+                            .add(details)
+                            .then((docRef) => {
+                                console.log("succesfully created game with id: ", docRef.id);
+                                return docRef.id
+                            })
+                            .catch((error) => {
+                                return error
+                            });
+                    }).catch(error => {
                         return error
-                    });
-            })
-        } else {
-            console.log("bbob")
-            return Promise.reject("Game name already exists")
-        }
-    }).catch(error => {
-        return error
-    })
+                    })
+            } else {
+                return Promise.reject("Game name already exists")
+            }
+        }).catch(error => {
+            return error
+        })
 }
 
 function joinGame(gameName: string, gamePassword: string) {
@@ -43,8 +43,29 @@ function leaveGame(gameId: string, userId: string): void {
     return;
 }
 
-function increasePlayerScore(gameId: string, userId: string, timeSinceRoundStart: number): void {
-    return;
+// Returns the updated score
+function increasePlayerScore(gameId: string, userId: string, percentRoundComplete: number): Promise<number> {
+    const gameDocRef = getFirebaseApp().firestore().collection("games").doc(gameId)
+    return getFirebaseApp().firestore().runTransaction((transaction) => {
+        return transaction.get(gameDocRef).then(gameDoc => {
+            if (!gameDoc.exists) {
+                return Promise.reject(`Game Id: ${gameId} is invalid`)
+            }
+
+            if (!gameDoc.data().leaderBoard.hasOwnProperty(userId)) {
+                return Promise.reject(`User Id: ${userId} is invalid`)
+            }
+            const newScore = (100 * percentRoundComplete) + gameDoc.data().leaderBoard[userId].score;
+            transaction.update(gameDocRef, {
+                [`leaderBoard.${userId}.score`]: newScore
+            });
+            return newScore
+        })
+    }).then(response => {
+        return response
+    }).catch(error => {
+        return error
+    })
 }
 
 export {
