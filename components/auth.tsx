486a657dae36089cb36ee2990f725c468d42847f
David <dnisenbaum1@gmail.com>
2020-05-15 22:09:47 -0400

    remove log

--- components/auth.tsx
+++ components/auth.tsx
@@ -37,7 +37,6 @@ export default class Auth extends Component<IProps, IState> {
         FirebaseApp.auth().onAuthStateChanged((user) => {
             let authenticated = false;
             let needsName = true;
-            console.log(user.uid);
             if (user?.displayName) {
                 console.log(`signed in as id: ${user.displayName}`)
                 if (this.props.onSuccess) {
